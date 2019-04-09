b3f8ea28abef8d620236e6c290f190ebae08b3fd
Joseonghyeon <nwilliterate@gmail.com>
2019-04-09 22:49:58 +0900

    FindCadidateFragment: Add Spinner Using ArrayAdapter, fragment_find_candidate.xml: Update ID

--- app/src/main/java/com/example/local_elections_app/View/FindCandidateFragment.java
+++ app/src/main/java/com/example/local_elections_app/View/FindCandidateFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -21,17 +22,31 @@ public class FindCandidateFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
          View v = inflater.inflate(R.layout.fragment_find_candidate, container, false);
 
-         // Spinner 선언
-        Spinner voteSpinner = (Spinner) v.findViewById(R.id.vote_spinner);
-        Spinner citySpinner = (Spinner) v.findViewById(R.id.city_spinner);
+        // Spinner 선언
+        Spinner typeCandidateSpinner = v.findViewById(R.id.types_candidate_spinner);
+        Spinner citySpinner = v.findViewById(R.id.city_spinner);
+        // Button 선언
         Button findBtn = v.findViewById(R.id.searchBtn);
+
+        // Spinner Using ArrayAdapter
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(), R.array.types_candidate, R.layout.custom_spinner_list);
+        adapter.setDropDownViewResource(R.layout.customer_spinner);
+        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(getContext(), R.array.city, R.layout.custom_spinner_list);
+        adapter2.setDropDownViewResource(R.layout.customer_spinner);
+        typeCandidateSpinner.setAdapter(adapter);
+        citySpinner.setAdapter(adapter2);
 
          return  v;
     }
