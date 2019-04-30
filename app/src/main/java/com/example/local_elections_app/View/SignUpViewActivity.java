e62b6dfbc4551a602f73efd6355ffd7bef03565f
Joseonghyeon <nwilliterate@gmail.com>
2019-05-01 00:26:18 +0900

    Activity: Add Sign Up View

---
+++ app/src/main/java/com/example/local_elections_app/View/SignUpViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.local_elections_app.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.local_elections_app.R;
+
+public class SignUpViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_view);
+    }
+}
