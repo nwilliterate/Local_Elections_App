83584292b3d85339e9da666880a931d00b71d6a9
dudcheol <95_pyc@naver.com>
2019-05-07 20:18:35 +0900

    Activity: Add SplashActivity

---
+++ app/src/main/java/com/example/local_elections_app/View/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.local_elections_app.View;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.local_elections_app.MainActivity;
+import com.example.local_elections_app.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
